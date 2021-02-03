93137716c76b415fe16a9fcccd75619d0fb02c5c
AnthonySantillan <58825544+AnthonySantillan@users.noreply.github.com>
2021-02-03 14:11:09 -0500

    examen final

---
+++ ExamenFinal/ClientApp/src/app/hero.ts
@@ -0,0 +1,4 @@
+export interface Santillan {
+  id: number;
+  name: string;
+}
---
+++ ExamenFinal/Hero.cs
@@ -0,0 +1,8 @@
+﻿namespace ExamenFinal.Controllers
+{
+    internal class Hero
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+    }
+}
